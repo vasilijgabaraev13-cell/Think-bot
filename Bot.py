8668558a3ce606ff31f529265921c24980f58a78
vasilijgabaraev13-cell <vasilijgabaraev13@gmail.com>
2026-06-15 17:59:36 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,1 +1,26 @@
 
+import vk_api
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+
+TOKEN = "vk1.a.E5qKhkZER6KRpNPf6wl2t7m2gQ04eRn7laKlfVypRuOAmkaXbwFK3qQ7ZRVbci2FGt7ygjxQl_xsuVWfzv2GIH2ZVwwzmR1qFFR7fhLJSXmJSKiofTsFuffeud8ZrhVB3zvQMvezLxoVPa2May5DO17vTTV7tYt7-3uT9FXTi3YM1oWP-xl4djqrzS6hSmgapYGRCxa5Uk9NRJKu-upSPQ"
+GROUP_ID = 239509472
+
+vk_session = vk_api.VkApi(token=TOKEN)
+longpoll = VkBotLongPoll(vk_session, GROUP_ID)
+vk = vk_session.get_api()
+
+print("Бот запущен!")
+
+# ЭТА ЧАСТЬ НИЖЕ — САМАЯ ВАЖНАЯ! БЕЗ НЕЁ БОТ НЕ ОТВЕЧАЕТ.
+for event in longpoll.listen():
+    if event.type == VkBotEventType.MESSAGE_NEW:
+        msg = event.object.message
+        user_id = msg['from_id']
+        text = msg['text'].lower()
+        
+        if text == "привет":
+            vk.messages.send(
+                user_id=user_id,
+                message="Привет! Я бот",
+                random_id=0
+            )
